3b2fce01ee587d95206b17064d119be3a61147e8
tad dy <a@a.com>
2024-09-08 00:08:10 -0400

    cleaner code

--- src/core/helpers/generate_signed_url.py
+++ src/core/helpers/generate_signed_url.py
@@ -3,21 +3,12 @@ import google.auth
 from google.oauth2 import service_account
 from datetime import datetime, timedelta
 import os
+from src.clients.gcs_client import GCSClient
 
 STORAGE_SERVICE_ACCOUNT_KEY = os.getenv("STORAGE_SERVICE_ACCOUNT_KEY")
 
 def generate_signed_url(bucket_name, file_name, expiration=3600):
-
-    if (os.getenv("ENVIRONMENT") == "prod"):
-        credentials, project = google.auth.default()
-    else:
-        """Return a Google Cloud Storage client using a specific service account."""
-        credentials = service_account.Credentials.from_service_account_file(
-        STORAGE_SERVICE_ACCOUNT_KEY
-        )
-        project = "kalygo-v3"
-
-    storage_client = storage.Client(credentials=credentials, project=project)
+    storage_client = GCSClient.get_storage_client()
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(file_name)
     expiration_time = datetime.now() + timedelta(seconds=expiration)
